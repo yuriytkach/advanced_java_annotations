64dbef4a30691cdb71b37306611ae9bf2639055a
Yuriy Tkach <yuriytkach@gmail.com>
2016-06-09 23:17:34 +0300

    Added some javadoc

--- src/main/java/ua/yet/adv/java/annotation/JavaBuildInAnnotations.java
+++ src/main/java/ua/yet/adv/java/annotation/JavaBuildInAnnotations.java
@@ -1,32 +1,36 @@
 package ua.yet.adv.java.annotation;
 
-@SuppressWarnings("unused")
+/**
+ * Sample class to showcase JDK's build-in annotations
+ * 
+ * @author Yuriy Tkach
+ */
 public class JavaBuildInAnnotations {
 
-	@Deprecated
-	private final int hello;
+    @Deprecated
+    private final int hello;
 
-	public JavaBuildInAnnotations(@Deprecated int hello) {
-		super();
-		this.hello = hello;
-	}
+    public JavaBuildInAnnotations(@Deprecated int hello) {
+        super();
+        this.hello = hello;
+    }
 
-	public void someMethod(@Deprecated int haha) {
+    public void someMethod(@Deprecated int haha) {
+        System.out.println(haha + hello);
+    }
 
-	}
+    @SafeVarargs
+    public final void varargs(final Object... args) {
+    }
 
-	@SafeVarargs
-	public final void varargs(final Object... args) {
-	}
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
-	@Override
-	public String toString() {
-		return super.toString();
-	}
-	
-	@FunctionalInterface
-	public static interface MyFunction {
-	    public String sayHi();
-	}
+    @FunctionalInterface
+    public static interface MyFunction {
+        public String sayHi();
+    }
 
 }
