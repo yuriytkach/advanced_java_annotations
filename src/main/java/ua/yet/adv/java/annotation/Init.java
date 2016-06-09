f519253829df9cab491e121ca16eec48592adafb
Yuriy Tkach <yuriytkach@gmail.com>
2016-06-09 23:23:03 +0300

    Added javadoc

--- src/main/java/ua/yet/adv/java/annotation/Init.java
+++ src/main/java/ua/yet/adv/java/annotation/Init.java
@@ -6,11 +6,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Own annotation to mark <code>init</code> methods that will be called when
+ * service is loaded
+ * 
+ * @author Yuriy Tkach
+ */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Init {
 
-	boolean suppressException() default false;
+    /**
+     * Specifies whether exception after execution of <code>init</code> method
+     * should be suppressed or not. Default is <code>false</code>, which means
+     * that it is expected that exceptions in <code>init</code> method are
+     * rethrown.
+     */
+    boolean suppressException() default false;
 
 }
--- src/main/java/ua/yet/adv/java/annotation/Service.java
+++ src/main/java/ua/yet/adv/java/annotation/Service.java
@@ -7,14 +7,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Own annotation to mark service classes that should be loaded and analyzed for
+ * <code>init</code> methods. The required attribute <code>name</code> and
+ * not-required <code>lazyLoad</code> are used to configure some properties.
+ * 
+ * @author Yuriy Tkach
+ */
 @Documented
 @Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Service {
 
-	String name();
+    /**
+     * Name of the service
+     */
+    String name();
 
-	boolean lazyLoad() default false;
+    /**
+     * Specifies, if the service should be loaded lazily. Default is
+     * <code>false</code>
+     */
+    boolean lazyLoad() default false;
 
 }
